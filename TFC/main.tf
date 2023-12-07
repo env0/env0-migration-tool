839d833a22234bdff2aa283e92920acedd0f74ec
Sagi Laufer <sagi.laufer@env0.com>
2023-12-07 11:51:46 +0200

    remove projects' map

--- TFC/main.tf
+++ TFC/main.tf
@@ -1,11 +1,4 @@
-locals {
-  data = jsonencode({
-    "workspaces" : local.workspaces,
-    "project_ids_to_names" : local.project_ids_to_names
-  })
-}
-
 resource "local_file" "data" {
-  content  = local.data
+  content  = jsonencode({ "workspaces" : local.workspaces })
   filename = "${path.module}/out/data.json"
 }
