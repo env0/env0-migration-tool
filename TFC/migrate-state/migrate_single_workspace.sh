80a972bea64883a8b0476fc5aeabb91280cb5b71
Sagi Laufer <sagi.laufer@env0.com>
2023-12-17 16:45:10 +0200

    declare WS_NAME at top of file, not using $1

--- TFC/migrate-state/migrate_single_workspace.sh
+++ TFC/migrate-state/migrate_single_workspace.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+WS_NAME=$1
+
 echo "-------"
-echo "Migrating state for workspace: $1"
+echo "Migrating state for workspace: $WS_NAME"
 echo "-------"
 
 # Create a directory based on WS_NAME
-ws_directory="./$1"
+ws_directory="./$WS_NAME"
 mkdir -p "$ws_directory"
 
 cd "$ws_directory" || exit
@@ -17,7 +19,7 @@ tfc_destination_file="tfc.main.tf"
 
 # Create a copy of the source file with replacements
 sed -e "s|!!!TFC_ORG_NAME!!!|${TFC_ORG_NAME}|g" \
-  -e "s|!!!WS_NAME!!!|$1|g" \
+  -e "s|!!!WS_NAME!!!|$WS_NAME|g" \
   "$tfc_source_file" >"$tfc_destination_file"
 
 echo "Initializing Terraform with TFC backend and pulling state"
@@ -40,7 +42,7 @@ env0_destination_file="env0.tf"
 # Create a copy of the source file with replacements
 sed -e "s|!!!ENV0_ORG_ID!!!|${ENV0_ORG_ID}|g" \
   -e "s|!!!ENV0_HOSTNAME!!!|${ENV0_HOSTNAME:="backend.api.env0.com"}|g" \
-  -e "s|!!!WS_NAME!!!|$1|g" \
+  -e "s|!!!WS_NAME!!!|$WS_NAME|g" \
   "$env0_source_file" >"$env0_destination_file"
 
 echo "Initializing Terraform with env0 backend and pushing state"
