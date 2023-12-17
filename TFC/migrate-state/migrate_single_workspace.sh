4f932e3bb6b84a798321d3eceeab3987707a6af2
Sagi Laufer <sagi.laufer@env0.com>
2023-12-17 15:12:12 +0200

    use a script, not a TF configuration to run migration

--- TFC/migrate-state/migrate_single_workspace.sh
+++ TFC/migrate-state/migrate_single_workspace.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
+set -e
 
-# Define the required environment variables
-required_env_vars=("ENV0_ORG_ID" "TFC_ORG_NAME" "WS_NAME" "ENV0_HOSTNAME")
-
-# Function to check if an environment variable is set
-check_env_var() {
-  if [ -z "${!1}" ]; then
-    echo "Error: $1 is not set."
-    exit 1
-  fi
-}
-
-# Check each required environment variable
-for env_var in "${required_env_vars[@]}"; do
-  check_env_var "$env_var"
-done
+echo "-------"
+echo "Migrating state for workspace: $1"
+echo "-------"
 
 # Create a directory based on WS_NAME
-ws_directory="./$WS_NAME"
+ws_directory="./$1"
 mkdir -p "$ws_directory"
-
-# Check if the directory was created successfully
-if [ $? -ne 0 ]; then
-  echo "Error creating directory: $ws_directory"
-  exit 1
-fi
 
 cd "$ws_directory" || exit
 
@@ -34,14 +17,8 @@ tfc_destination_file="tfc.main.tf"
 
 # Create a copy of the source file with replacements
 sed -e "s|!!!TFC_ORG_NAME!!!|${TFC_ORG_NAME}|g" \
-  -e "s|!!!WS_NAME!!!|${WS_NAME}|g" \
+  -e "s|!!!WS_NAME!!!|$1|g" \
   "$tfc_source_file" >"$tfc_destination_file"
-
-# Check if the destination file was created successfully
-if [ $? -ne 0 ]; then
-  echo "Error creating file: $tfc_destination_file"
-  exit 1
-fi
 
 echo "Initializing Terraform with TFC backend and pulling state"
 
@@ -63,14 +40,8 @@ env0_destination_file="env0.tf"
 # Create a copy of the source file with replacements
 sed -e "s|!!!ENV0_ORG_ID!!!|${ENV0_ORG_ID}|g" \
   -e "s|!!!ENV0_HOSTNAME!!!|${ENV0_HOSTNAME}|g" \
-  -e "s|!!!WS_NAME!!!|${WS_NAME}|g" \
+  -e "s|!!!WS_NAME!!!|$1|g" \
   "$env0_source_file" >"$env0_destination_file"
-
-# Check if the destination file was created successfully
-if [ $? -ne 0 ]; then
-  echo "Error creating file: $env0_destination_file"
-  exit 1
-fi
 
 echo "Initializing Terraform with env0 backend and pushing state"
 
