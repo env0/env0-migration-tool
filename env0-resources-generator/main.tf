7371f7220e768a10107e4ab86bc879463a716c8f
Asaf Blubshtein <57428051+asafsb@users.noreply.github.com>
2024-04-16 22:55:53 -0500

    Update main.tf
    add token id variable

--- env0-resources-generator/main.tf
+++ env0-resources-generator/main.tf
@@ -8,7 +8,7 @@ resource "local_file" "main" {
 }
 
 resource "local_file" "environments" {
-  content  = templatefile("terraform_templates/environments.tftpl", { workspaces = var.workspaces })
+  content  = templatefile("terraform_templates/environments.tftpl", { workspaces = var.workspaces , ado_token_id = var.ado_token_id})
   filename = "${path.module}/out/environments.tf"
 
   provisioner "local-exec" {
