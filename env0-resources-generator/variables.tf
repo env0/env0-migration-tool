ee17c859cc8e61b32ce922ceb46cae5d213a3612
Asaf Blubshtein <57428051+asafsb@users.noreply.github.com>
2024-04-16 22:56:51 -0500

    Update variables.tf
    add ado_token_id variable

--- env0-resources-generator/variables.tf
+++ env0-resources-generator/variables.tf
@@ -1,3 +1,8 @@
 variable "workspaces" {
   description = "The list of workspaces exported. Should be set like this, for example: -var-file=../TFC/out/data.json"
 }
+
+variable "ado_token_id" {
+  description = "env0 token id of the ADO repos"
+  default = ""
+}
